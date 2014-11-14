ba57c9ebaf745021a9c6d02915caf598dcf5a753
Matt Donnelly <mattdonnelly@me.com>
2014-11-14 02:32:41 +0000

    Added basic motion and edge detection

--- Lab 2/Lab 2/main.cpp
+++ Lab 2/Lab 2/main.cpp
@@ -7,10 +7,81 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#define ESC_KEY 27
+
+static cv::Mat initial_frame;
+
+bool containsMotion(cv::Mat current_frame, cv::Mat previous_frame) {
+    cv::Mat diff;
+    cv::absdiff(current_frame, previous_frame, diff);
+    
+    cvtColor(diff, diff, CV_RGB2GRAY);
+    cv::threshold(diff, diff, 35, 255, CV_THRESH_BINARY);
+    
+    double percentage = (cv::countNonZero(diff) / (double)diff.total()) * 100;
+
+    return percentage > 2.0;
+}
+
+cv::Mat findEdges(cv::Mat frame) {
+    cv::Mat frame_gray, canny_output, detected_edges;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+
+    cv::GaussianBlur(frame, frame, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+    
+    cv::cvtColor(frame, frame_gray, CV_RGB2GRAY);
+    
+    cv::Canny(frame_gray, canny_output, 100, 150);
+    cv::findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+    cv::Mat edges = cv::Mat::zeros( canny_output.size(), CV_8UC3 );
+    for (int i = 0; i < contours.size(); i++) {
+        drawContours(edges, contours, i, cv::Scalar(255, 255, 255), 1, 8, hierarchy, 0, cv::Point());
+    }
+
+    return edges;
+}
+
+void checkPostboxesForFrame(cv::Mat current_frame) {
+    if (!containsMotion(current_frame, initial_frame)) {
+        cv::Mat edges = findEdges(current_frame);
+        
+        cv::imshow("Edges", edges);
+    }
+    else {
+        initial_frame = current_frame.clone();
+    }
+}
+
+int main(int argc, char **argv) {
+    cv::VideoCapture cap("/Users/mattdonnelly/Documents/College/Computer Vision/Lab 2/Media/Postboxes.avi");
+    if (!cap.isOpened()) {
+        return -1;
+    }
+    
+    const double fps = cap.get(CV_CAP_PROP_FPS);
+    
+    cv::Mat current_frame;
+    while (true) {
+        if (!cap.read(current_frame)) {
+            std::cout << "Unable to read frame from video capture." << std::endl;
+            break;
+        }
+        
+        if (initial_frame.empty()) {
+            initial_frame = current_frame.clone();
+        }
+        
+        checkPostboxesForFrame(current_frame);
+        
+        if (cv::waitKey(1000.0 / fps) == ESC_KEY) {
+            break;
+        }
+    }
+    
+    cap.release();
 }
