970f0d70c59fdd1a6fc7a23b2204005897462597
Matt Donnelly <mattdonnelly@me.com>
2014-12-03 01:44:30 +0000

    Added Lab 3

---
+++ Lab 3/Lab 3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Lab 3
+//
+//  Created by Matt Donnelly on 02/12/2014.
+//  Copyright (c) 2014 Matt Donnelly. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
