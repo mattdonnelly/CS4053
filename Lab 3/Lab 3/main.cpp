0e58d4f7e4cba8582b7e8fa0b5812783661b1fc1
Matt Donnelly <mattdonnelly@me.com>
2014-12-11 22:45:14 +0000

    idk stuff who cares at this point

--- Lab 3/Lab 3/main.cpp
+++ Lab 3/Lab 3/main.cpp
@@ -9,15 +9,16 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-#define MIN_HUE_RANGE 0.0f
-#define MAX_HUE_RANGE 55.0f
+#define MIN_LAB_RANGE 30.0f
+#define MAX_LAB_RANGE 180.0f
 
-#define MIN_SAT_RANGE 150.0f
-#define MAX_SAT_RANGE 255.0f
+#define MIN_A_RANGE 137.0f
+#define MAX_A_RANGE 255.0f
 
-#define MAX_BLUE_VALUE 90.0f
-#define MAX_GREEN_VALUE 68.0f
-#define MIN_RED_VALUE 57.0f
+#define MIN_B_RANGE 100.0f
+#define MAX_B_RANGE 255.0f
+
+#define BLUR_AMOUNT 5.0f
 
 #define CLOSE_AMOUNT 5.0f
 #define OPEN_AMOUNT 7.0f
@@ -30,16 +31,17 @@ cv::Mat find_red_areas(cv::Mat img) {
     cv::Mat lab;
     cv::cvtColor(img, lab, CV_BGR2Lab);
 
-    cv::GaussianBlur(lab, lab, cv::Size(5, 5), 20.0);
+    cv::GaussianBlur(lab, lab, cv::Size(BLUR_AMOUNT, BLUR_AMOUNT), 20.0);
     
     cv::Mat result;
-    cv::inRange(lab, cv::Scalar(0, 137, 100), cv::Scalar(180, 255, 255), result);
-    
-    cv::erode(result, result, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(CLOSE_AMOUNT, CLOSE_AMOUNT)));
-    cv::dilate(result, result, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(CLOSE_AMOUNT, CLOSE_AMOUNT)));
-    
-    cv::dilate(result, result, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(OPEN_AMOUNT, OPEN_AMOUNT)));
-    cv::erode(result, result, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(OPEN_AMOUNT, OPEN_AMOUNT)));
+    cv::inRange(lab,
+                cv::Scalar(MIN_LAB_RANGE, MIN_A_RANGE, MIN_B_RANGE),
+                cv::Scalar(MAX_LAB_RANGE, MAX_A_RANGE, MAX_B_RANGE),
+                result);
+
+    cv::morphologyEx(result, result, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(CLOSE_AMOUNT, CLOSE_AMOUNT)));
+
+    cv::morphologyEx(result, result, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(CLOSE_AMOUNT, OPEN_AMOUNT)));
 
     return result;
 }
@@ -105,10 +107,10 @@ cv::Mat extract_shape(cv::Mat img, Contour contour) {
     cv::drawContours(mask, contour_vec, 0, cv::Scalar(255), CV_FILLED);
 
     cv::Mat imageROI;
-    img.copyTo(imageROI, mask);
-    cv::Mat contour_region = imageROI(bounding_rect);
+    //img.copyTo(imageROI, mask);
+    cv::Mat contour_region = img(bounding_rect);
     
-    cv::RotatedRect rotated_rect = cv::minAreaRect(cv::Mat(contour));
+    /*cv::RotatedRect rotated_rect = cv::minAreaRect(cv::Mat(contour));
 
     cv::Point2f src_points[4];
     rotated_rect.points(src_points);
@@ -127,33 +129,135 @@ cv::Mat extract_shape(cv::Mat img, Contour contour) {
     cv::Mat warp_mat = getAffineTransform(src_points, dst_points);
     
     cv::Mat warped = cv::Mat::zeros(contour_region.rows, contour_region.cols, contour_region.type());
-    cv::warpAffine(contour_region, warped, warp_mat, warped.size());
+    cv::warpAffine(contour_region, warped, warp_mat, warped.size());*/
 
-    return warped;
+    return contour_region;
 }
 
-cv::Mat matching_sign(cv::Mat img, std::vector<cv::Mat> samples) {
-    cv::Mat best_match = cv::Mat::zeros(cvSize(1024, 1024), CV_8UC3);
-    
-    for (int i = 0; i < samples.size(); i++) {
-        /*cv::Mat result;
-        matchTemplate(img, samples[i], result, CV_TM_SQDIFF);
-        normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+void chamfer_matching(cv::Mat &chamfer_image, cv::Mat &model, cv::Mat &matching_image) {
+    // Extract the model points (as they are sparse).
+    std::vector<cv::Point> model_points;
+    int image_channels = model.channels();
+    for (int model_row = 0; model_row < model.rows; model_row++) {
+        uchar *curr_point = model.ptr<uchar>(model_row);
         
-        cv::imshow("hi", result);
-        cv::waitKey(-1);*/
+        for (int model_column = 0; model_column < model.cols; model_column++) {
+            if (*curr_point > 0) {
+                cv::Point new_point = cv::Point(model_column,model_row);
+                model_points.push_back(new_point);
+            }
+
+            curr_point += image_channels;
+        }
     }
     
-    return best_match;
+    size_t num_model_points = model_points.size();
+    image_channels = chamfer_image.channels();
+    
+    // Try the model in every possible position
+    matching_image = cv::Mat(chamfer_image.rows-model.rows+1, chamfer_image.cols-model.cols+1, CV_32FC1);
+    
+    for (int search_row=0; search_row <= chamfer_image.rows-model.rows; search_row++) {
+        float *output_point = matching_image.ptr<float>(search_row);
+        
+        for (int search_column = 0; search_column <= chamfer_image.cols-model.cols; search_column++) {
+            float matching_score = 0.0;
+            
+            for (int point_count = 0; point_count < num_model_points; point_count++) {
+                matching_score += (float) *(chamfer_image.ptr<float>(model_points[point_count].y+search_row) + search_column + model_points[point_count].x*image_channels);
+            }
+            
+            *output_point = matching_score;
+            output_point++;
+        }
+    }
+}
+
+void show_32bit_image(const char *window_name, cv::Mat &passed_image, double zero_maps_to = 0.0, double passed_scale_factor =-1.0 )
+{
+    cv::Mat display_image;
+    double scale_factor = passed_scale_factor;
+    if (passed_scale_factor == -1.0)
+    {
+        double minimum,maximum;
+        cv::minMaxLoc(passed_image,&minimum,&maximum);
+        scale_factor = (255.0-zero_maps_to)/cv::max(-minimum,maximum);
+    }
+    passed_image.convertTo(display_image, CV_8U, scale_factor, zero_maps_to);
+    imshow( window_name, display_image );
+}
+
+cv::Mat aspect_fit_in_image(cv::Mat img, cv::Size size) {
+    double ratios[2] = {
+        (double)size.width / (double)img.cols,
+        (double)size.height / (double)img.rows
+    };
+    
+    cv::Size resize(img.cols, img.rows);
+    if (ratios[0] < ratios[1]) {
+        resize.width = (int)(resize.width * ratios[0] + 1);
+        resize.height = (int)(resize.height * ratios[0] + 1);
+    }
+    else {
+        resize.width = (int)(resize.width * ratios[1] + 1);
+        resize.height = (int)(resize.height * ratios[1] + 1);
+    }
+    
+    cv::Mat result;
+    cv::resize(img, result, resize);
+    
+    return result;
+}
+
+void matching_sign(cv::Mat img, std::vector<cv::Mat> samples) {
+    cv::Mat best_match = cv::Mat::zeros(cvSize(1024, 1024), CV_8UC3);
+    
+    cv::imshow("Original", img);
+    
+    cv::Mat img_edges, chamfer_image;
+    cv::cvtColor(img, img_edges, CV_BGR2GRAY);
+    cv::Canny(img_edges, img_edges, 200, 300, 3);
+    cv::threshold(img_edges, img_edges, 127, 255, cv::THRESH_BINARY);
+    cv::distanceTransform(img_edges, chamfer_image, CV_DIST_L2 , 3);
+
+    show_32bit_image("Chamfer image", chamfer_image);
+    cv::waitKey(-1);
+    
+    for (int i = 0; i < samples.size(); i++) {
+        cv::Mat sample = samples[i].clone();
+        cv::imshow("Matching against", sample);
+        
+        cv::cvtColor(sample, sample, CV_BGR2GRAY);
+        cv::Canny(sample, sample, 200, 400, 3);
+        cv::threshold(sample, sample, 127, 255, cv::THRESH_BINARY);
+
+        if (sample.size().width > chamfer_image.size().width ||
+               sample.size().height > chamfer_image.size().height) {
+            sample = aspect_fit_in_image(sample, chamfer_image.size());
+        }
+        
+        cv::Mat result;
+        chamfer_matching(chamfer_image, sample, result);
+
+        std::cout << result.size() << std::endl;
+        
+        if (result.size().area()) {
+            show_32bit_image("Matching image", result);
+            cv::waitKey(-1);
+        }
+    }
 }
 
 void draw_shape_rects(cv::Mat img, std::vector<Contour> contours) {
+    static cv::RNG rng(12345);
+    
     for (int i = 0; i < contours.size(); i++) {
         cv::RotatedRect rect = cv::minAreaRect(cv::Mat(contours[i]));
         
-        static cv::RNG rng(12345);
         cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        cv::Point2f rect_points[4]; rect.points( rect_points );
+        cv::Point2f rect_points[4];
+        rect.points( rect_points );
+        
         for (int j = 0; j < 4; j++ ) {
             line(img, rect_points[j], rect_points[(j+1)%4], color, 2, 8);
         }
@@ -200,9 +304,10 @@ int main(int argc, const char * argv[]) {
         cv::Mat red_area = find_red_areas(img);
         std::vector<Contour> contours = find_blob_contours(red_area);
         
-        draw_shape_rects(img, contours);
-        cv::imshow("Hi", img);
-        cv::waitKey(-1);
+        for (int j = 0; j < contours.size(); j++) {
+            cv::Mat sign = extract_shape(img, contours[j]);
+            matching_sign(sign, known_images);
+        }
     }
     
     return 0;
